5c8662d443248d35bfb440f7f9cf7ccc4129f2b4
JK117 <sheppard.jk@outlook.com>
2022-12-01 14:43:28 +0800

    :wrench: Configured alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      // src: require('path').resolve(__dirname, 'src/')
+      'src' : path.resolve(__dirname, './src'),
+      'components': path.resolve(__dirname, './src/components')
+    }
+  },
   plugins: [vue()],
   server: {
     port: 8080,
