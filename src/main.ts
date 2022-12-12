031b0c58a2194c7b7ff40db15013c15bd47c99d2
JK117 <sheppard.jk@outlook.com>
2022-12-12 16:14:28 +0800

    :lipstick: Restructured scss chain

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
-// import './style.css'
-import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
