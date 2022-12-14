c74b7df296bf612e75906deae3de69cfef023bc7
JK117 <sheppard.jk@outlook.com>
2022-12-14 16:20:34 +0800

    :construction:

---
+++ src/components/card-flow/ImageCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+</script>
+
+
+<template>
+    <el-card>
+        <img src="" alt="">
+    </el-card>
+</template>
+
+
+<style scoped lang="scss"></style>
--- src/components/views/CardFlow.vue
+++ src/components/views/CardFlow.vue
@@ -1,8 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from 'axios'
+import { ref, onMounted } from 'vue';
+import MainContentFrame from '@/components/views/MainContentFrame.vue'
+import ImageCard from '@/components/card-flow/ImageCard.vue';
+
+const image_list = ref([])
+onMounted (async ()=> {
+    const res = await axios.get("https://api.thecatapi.com/v1/images/search?limit=10")
+    image_list.value = res.data
+})
+</script>
 
 
 <template>
-    <div>
+    <MainContentFrame>
         <h1>Card Flow Template</h1>
-    </div>
+    </MainContentFrame>
 </template>
+
+
+<style scoped lang="scss"></style>
